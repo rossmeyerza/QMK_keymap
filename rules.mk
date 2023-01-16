34d6593b99cd43b2509b3bcd3ce351a7afd06828
rossmeyerza <rossmeyerza@gmail.com>
2023-01-16 19:36:25 +0000

    initial commit

---
+++ rules.mk
@@ -0,0 +1,14 @@
+OLED_ENABLE = yes
+OLED_DRIVER = SSD1306   # Enables the use of OLED displays
+ENCODER_ENABLE = no       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+WPM_ENABLE = yes
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+AUDIO_ENABLE = no
+MOUSEKEY_ENABLE = yes
+EXTRAFLAGS += -flto
+MIDI_ENABLE = no
+UNICODE_ENABLE   = no# Unicode
+SWAP_HANDS_ENABLE= no
+RAW_ENABLE = yes
